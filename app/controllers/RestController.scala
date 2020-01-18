3e089b0c65fe1e0bf41712bfa495ea31a12b1703
Jaromir <jaromircharles@hotmail.com>
2020-01-18 23:41:33 +0100

    WIP: functionality works, fix layout

--- app/controllers/RestController.scala
+++ app/controllers/RestController.scala
@@ -7,6 +7,8 @@ import play.api.mvc._
 import de.scala_quest.controller.defaultImpl.Controller
 import de.scala_quest.model.Player
 import de.scala_quest.view.Ui
+import play.api.data.Form
+import play.api.data.Forms._
 
 @Singleton
 class RestController @Inject() (
@@ -16,7 +18,37 @@ class RestController @Inject() (
 
   serverCtrl.addObserver(this)
 
-  def onStartGame: Action[AnyContent] = Action {
+  def test(): Action[AnyContent] = Action {
+    println("RestController.testPost() with name: ")
+    Ok(views.html.test("Received name: "))
+  }
+
+  def doLogin = Action { implicit request =>
+    val loginRequest = loginForm.bindFromRequest.get
+    Ok(s"username: '${loginRequest.player1}', password: '${loginRequest.player2}'")
+  }
+
+  case class LoginRequest(player1:String, player2:String)
+
+  def loginForm = Form(mapping("player1" -> text, "player2" -> text)
+  (LoginRequest.apply)(LoginRequest.unapply))
+
+  def onAddPlayers: Action[AnyContent] =  Action { implicit request =>
+    val loginRequest = loginForm.bindFromRequest.get
+    serverCtrl.addNewPlayerToGame(loginRequest.player1)
+    serverCtrl.addNewPlayerToGame(loginRequest.player2)
+    println("Players: " + serverCtrl.getPlayers())
+    Ok(s"first name: '${loginRequest.player1}', second name: '${loginRequest.player2}'")
+  }
+
+
+
+  def onStartGame: Action[AnyContent] = Action {implicit request =>
+    val loginRequest = loginForm.bindFromRequest.get
+
+    serverCtrl.addNewPlayerToGame(loginRequest.player1)
+    serverCtrl.addNewPlayerToGame(loginRequest.player2)
+    println("Players: " + serverCtrl.getPlayers())
     serverCtrl.startGame()
     /* get roundNr, current player, points current question and current answers */
     val roundNr = serverCtrl.getRoundNr()
@@ -56,15 +88,22 @@ class RestController @Inject() (
   def onAddNewPlayerToGame(name: String): Action[AnyContent] =  Action {
     serverCtrl.addNewPlayerToGame(name)
     val res = serverCtrl.getPlayerNames()
-    println("res: "+ res)
-    //println("here")
-    //NotFound("I received post data")
     Ok(views.html.test(name))
     Ok(views.html.addPlayers("New Game...", name, style ="scala"))
   }
 
-  def onProcessAnswer(input: String): Action[AnyContent] = Action {
-    serverCtrl.processAnswer(input.toInt)
+  case class PossibleAnswers(answer: String)
+
+  def answerForm: Form[PossibleAnswers] = Form(mapping(
+    "answer" -> text)
+  (PossibleAnswers.apply)(PossibleAnswers.unapply))
+
+
+  def onProcessAnswer: Action[AnyContent] = Action {implicit request =>
+    val userInput = answerForm.bindFromRequest.get
+
+    Ok(s"ans1: '${userInput.answer}'")
+    serverCtrl.processAnswer(1)
 
     val roundNr = serverCtrl.getRoundNr()
     val (playersName, playersPoints) = serverCtrl.getPlayerInfo()
@@ -110,6 +149,10 @@ class RestController @Inject() (
     println("RestController.testPost() with name: " + name)
     Ok(views.html.test("Received name: " + name))
   }
+
+
+
+
 
   override def update(updateData: GameState): Unit = {
 
