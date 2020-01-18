8dceb0b3d2c9ef698508a412ecf876aeb28da982
Jaromir <jaromircharles@hotmail.com>
2020-01-18 19:56:02 +0100

    WIP: get post to send correct data

--- app/controllers/RestController.scala
+++ app/controllers/RestController.scala
@@ -5,6 +5,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 import play.api.mvc._
 import de.scala_quest.controller.defaultImpl.Controller
+import de.scala_quest.model.Player
 import de.scala_quest.view.Ui
 
 @Singleton
@@ -17,7 +18,28 @@ class RestController @Inject() (
 
   def onStartGame: Action[AnyContent] = Action {
     serverCtrl.startGame()
-    Ok(views.html.gamePlay("ScalaQuest"))
+    /* get roundNr, current player, points current question and current answers */
+    val roundNr = serverCtrl.getRoundNr()
+    val (playersName, playersPoints) = serverCtrl.getPlayerInfo()
+    val currentQuestion = serverCtrl.getPlayersCurrentQuestion()
+    val currentAnswers = serverCtrl.getPlayersCurrentAnswers()
+
+    println("roundNr: " + roundNr)
+    println("playersName: " + playersName)
+    println("playersPoints: " + playersPoints)
+    println("currentQuestion: " + currentQuestion)
+    println("currentAnswers: " + currentAnswers)
+
+    Ok(views.html.gamePlay(
+      "ScalaQuest",
+      roundNr,
+      playersName,
+      playersPoints,
+      currentQuestion,
+      currentAnswers,
+      style ="scala"
+      )
+    )
   }
 
   def onQuitGame: Action[AnyContent] = Action {
@@ -27,14 +49,49 @@ class RestController @Inject() (
 
   def onNewGame: Action[AnyContent] = Action {
     serverCtrl.newGame()
-    Ok(views.html.addPlayers("New Game...", style = "scala"))
+    println("RestController.onNewGame")
+    Ok(views.html.addPlayers("New Game...", "",style = "scala"))
   }
 
-  def onAddNewPlayerToGame(name: String): Action[AnyContent] = Action {
+  def onAddNewPlayerToGame(name: String): Action[AnyContent] =  Action {
     serverCtrl.addNewPlayerToGame(name)
-    println("here")
-    Ok("I received post data")
-    //Ok(views.html.test(name))
+    val res = serverCtrl.getPlayerNames()
+    println("res: "+ res)
+    //println("here")
+    //NotFound("I received post data")
+    Ok(views.html.test(name))
+    Ok(views.html.addPlayers("New Game...", name, style ="scala"))
+  }
+
+  def onProcessAnswer(input: String): Action[AnyContent] = Action {
+    serverCtrl.processAnswer(input.toInt)
+
+    val roundNr = serverCtrl.getRoundNr()
+    val (playersName, playersPoints) = serverCtrl.getPlayerInfo()
+    val currentQuestion = serverCtrl.getPlayersCurrentQuestion()
+    val currentAnswers = serverCtrl.getPlayersCurrentAnswers()
+
+    println("roundNr: " + roundNr)
+    println("playersName: " + playersName)
+    println("playersPoints: " + playersPoints)
+    println("currentQuestion: " + currentQuestion)
+    println("currentAnswers: " + currentAnswers)
+
+    if(roundNr < 3) {
+      Ok(views.html.gamePlay(
+        "ScalaQuest",
+        roundNr,
+        playersName,
+        playersPoints,
+        currentQuestion,
+        currentAnswers,
+        style = "scala"
+      )
+      )
+    } else {
+      val players: List[Player] = serverCtrl.getPlayers()
+      Ok(views.html.displayResults("Results", players, style = "scala"))
+    }
   }
 
   def onGetRoundNr(): Action[AnyContent] = Action {
@@ -49,8 +106,9 @@ class RestController @Inject() (
     Ok(views.html.test("onGetPlayersCurrentQuestion"))
   }
 
-  def testPost(): Action[AnyContent] = Action {
-    Ok(views.html.test("I received post data"))
+  def testPost(name: String): Action[AnyContent] = Action {
+    println("RestController.testPost() with name: " + name)
+    Ok(views.html.test("Received name: " + name))
   }
 
   override def update(updateData: GameState): Unit = {
